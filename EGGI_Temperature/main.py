ebb30e4f9a2ff612c042fce919ad5a084d2c74e7
wwlouis00 <64067917+wwlouis00@users.noreply.github.com>
2021-11-19 03:15:46 +0800

    Update main.py

--- EGGI_Temperature/main.py
+++ EGGI_Temperature/main.py
@@ -1,6 +1,5 @@
-import sys
 from PyQt5.QtWidgets import QApplication,QMainWindow
-from Sulink_Temperature import *
+from Sulink_Temperature_20211118 import *
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
