f60fe34cac3c7d5a6242c1276f879add18903c5e
unknown <wwlouis00@gmail.com>
2021-12-02 18:17:28 +0800

    update 2021/12/02

---
+++ QtDesigner/GridLayout.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt5.QtWidgets import QApplication,QFileDialog,QWidget,QGridLayout,QPushButton
+
+class Winform(QWidget):
+    def __init__(self,parent=None):
+        super(Winform, self).__init__(parent)
+        self.initUI()
+
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        names = ['Cls','Back','','Close',
+                 '7','8','9','/',
+                 '4', '5', '6', '*',
+                 '1', '2', '3', '-',
+                 '0', '.', '=', '+',]
+        positions = [(i,j) for i in range(5) for j in range(4)]
+
+        for position, name in zip(positions,names):
+            if name == '':
+                continue
+            button = QPushButton(name)
+            grid.addWidget(button, * position)
+        self.move(300,150)
+        self.setWindowTitle("格子布局管理範例")
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    form = Winform()
+    form.show()
+    sys.exit(app.exec_())
